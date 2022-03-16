7038a87f956ed21ee3589080e36202e04ab7ec93
Ramirez-Nicolas <98922442+Ramirez-Nicolas@users.noreply.github.com>
2022-03-16 01:51:38 +0100

    Initial commit

---
+++ include/my_macro_abs.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** my_macro_abs.h
+** File description:
+** 
+*/
+
+#ifndef MY_MACRO_ABS_H
+#define MY_MACRO_ABS_H
+
+#define ABS(value) ((value >= 0) ? (value) : (-value))
+
+#endif /* MY_MACRO_ABS_H */
